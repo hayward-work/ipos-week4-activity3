38ae8ce96f557c52a0b3469a14e1dd69c8ca3988
20125011 <20125011@tafe.wa.edu.au>
2025-08-13 16:15:51 +0800

    test: implement project tests

--- test/test_project.py
+++ test/test_project.py
@@ -1,11 +1,22 @@
 # test_project.py
 import unittest
+from src.project import project as p
 
+class TestProject(unittest.TestCase):
+
+    def testAddContracts(self):
+        self.project.addContract(contract = self.Contract(name = "name", enail="email"))
+        self.assertTrue(self.project.contacts.contains("testContact"))
 
 
     # Step 4
 
         # Create an organization
+    def setUp(self):
+        self.project = p(name = "name", organisation = "organisation", contacts = ["contact1", "contact2"])
+
+    def initialiseProjectWithAllAttributes(self):
+        self.assertTrue(hasattr(self.project, "name" and "organisation" and "contacts"))
 
         # Create a project associated with the organisation
 
@@ -19,3 +30,6 @@ import unittest
 
     # Challenge test that you can add a contact to an organisation
     # and a project using the project class
+
+    def tearDown(self):
+        self.project.destroy()
